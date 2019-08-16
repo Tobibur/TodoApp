bf11ab8189634dd6821867bb3984e462e9d26819
Tobibur Rahman <tobiburrahman786@gmail.com>
2019-08-16 10:04:59 +0530

    changed urlpatterns from url to path

--- todos/urls.py
+++ todos/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^categories/$', views.ListCreateCategories.as_view(), name="categories-all"),
-    url(r'^categories/(?P<pk>\d+)/$', views.RetrieveUpdateDestroyCategory.as_view(), name="category_detail"),
-    url(r'^todo/$', views.ListCreateTodo.as_view(), name="todo-all"),
-    url(r'^todo/(?P<pk>\d+)/$', views.RetrieveUpdateDestroyTodo.as_view(), name="todo_detail"),
+    path('', views.index, name='index'),
+    path('categories/', views.ListCreateCategories.as_view(), name="categories-all"),
+    path('categories/<int:pk>/', views.RetrieveUpdateDestroyCategory.as_view(), name="category_detail"),
+    path('todo/', views.ListCreateTodo.as_view(), name="todo-all"),
+    path('todo/<int:pk>/', views.RetrieveUpdateDestroyTodo.as_view(), name="todo_detail"),
 ]
